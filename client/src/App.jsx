22c3355a8b1e09f8035155fa54e5b7ec81b54f7d
HectorRiahno <hjrianol10@gmail.com>
2025-10-23 15:12:32 -0500

    GP-35 Desarrollar CRUD completo de proveedores en el Frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import ResetPasswordPage from './pages/ResetPassword'
 import { ThemeProvider } from './components/ui/theme-provider'
 import UsersListPage from './pages/UsersList'
 import ProductsListPage from './pages/ProductsList'
+import SuppliersListPage from './pages/SupplierList'
 import Dashboard from './pages/Dashboard'
 import { Toaster } from './components/ui/toaster'
 import { ProtectedRoute } from './components/ui/ProtectedRoute'
@@ -28,6 +29,7 @@ function App() {
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/users" element={<UsersListPage />} />
             <Route path="/products" element={<ProductsListPage />} />
+            <Route path="/suppliers" element={<SuppliersListPage/>} />
             <Route path="/profile" element={<div>Perfil del Usuario</div>} />
             <Route path="/suppliers" element={<div>Gestión de Proveedores</div>} />
             <Route path="/employees" element={<div>Gestión de Empleados</div>} />
